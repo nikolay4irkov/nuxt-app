a7e675d18a98d70a07b2b76b912b2f1bfb89df6b
nikolaychirkov <nikolay4irkov@gmail.com>
2021-04-25 17:36:48 +0300

    moved axios to separate service

---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <div class="">
+      {{ message }}
+    </div>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  head() {
+    return {
+      title: this.message,
+    }
+  },
+  computed: {
+    statusCode() {
+      return (this.error && this.error.statusCode) || 500
+    },
+    message() {
+      return this.error.message
+    },
+  },
+}
+</script>
---
+++ pages/event/_id.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+    <h1>{{ event.title }}</h1>
+    <h3>{{ event.description }}</h3>
+    <h3>{{ event.time }}</h3>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+export default {
+  async asyncData({ $axios, error, params }) {
+    try {
+      const { data } = await EventService.getEvent(params.id)
+      return { event: data }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch event' + params.id,
+      })
+    }
+  },
+}
+</script>
+
+<style></style>
